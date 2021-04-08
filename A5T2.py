fbb4262dd911954bf8bd88552a1bbe4d80a5917f
ccBrita <xuanhao@ualberta.ca>
2021-04-08 06:16:30 -0600

    fix A5T2

--- A5T2.py
+++ A5T2.py
@@ -1,31 +1,31 @@
 from pymongo import MongoClient
 import csv
 
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb://localhost:27017")
 #connect to mongo serve
 mydb = client["A5db"]
 
-def create_listing(name, id1, host_id, host_name, neighbourhood, room_type, price, minimum_nights, availability_365,lines):
+def create_listing(name, id1, host_id, host_name, neighbourhood, room_type, price, minimum_nights, availability_365):
     global client
     global mydb
     col = mydb["listing"]
     i = 0
-    while (i < lines[0]):
+    while (i < len(id1)):
         listing = {"id":id1[i], "name":name[i], "host_id":host_id[i], "host_name":host_name[i], "neighbourhood":neighbourhood[i], "room_type":room_type[i], "price":price[i], "minmum_nights":minimum_nights[i],"availability_365": availability_365[i]}
         col.insert_one(listing)
         i += 1
     
-def create_reviews(listing_id, id2, date, reviewer_id, reviewer_name, comments, lines):
+def create_reviews(listing_id, id2, date, reviewer_id, reviewer_name, comments):
     global client
     global mydb
     col = mydb["reviews"]
     i = 0
-    while (i < lines[1]):
+    while (i < len(id2)):
         review = {"listing_id":listing_id[0], "id":id2[i], "date":date[i], "reviewer_id":reviewer_id[i], "reviewer_name":reviewer_name[i], "comments":comments[i]}
         col.insert_one(review)
         i += 1
 
-def read_csv(name, id1, host_id, host_name, neighbourhood, room_type, price, minimum_nights, availability_365, listing_id, id2, date, reviewer_id, reviewer_name, comments, lines):
+def read_csv(name, id1, host_id, host_name, neighbourhood, room_type, price, minimum_nights, availability_365, listing_id, id2, date, reviewer_id, reviewer_name, comments):
     file1 = open("YVR_Airbnb_listings_summary.csv","r")
     reader1 = csv.reader(file1)
     for i in reader1:
@@ -40,7 +40,6 @@ def read_csv(name, id1, host_id, host_name, neighbourhood, room_type, price, min
         price.append(i[6])
         minimum_nights.append(i[7])
         availability_365.append(i[8])
-    lines.append(reader1.line_num-1)
     file1.close()
     file2 = open("YVR_Airbnb_reviews.csv","r")
     reader2 = csv.reader(file2)
@@ -53,7 +52,6 @@ def read_csv(name, id1, host_id, host_name, neighbourhood, room_type, price, min
         reviewer_id.append(i[3])
         reviewer_name.append(i[4])
         comments.append(i[5])
-    lines.append(reader2.line_num-1)
     file2.close()
 
     
@@ -74,10 +72,9 @@ def main():
     reviewer_id = []
     reviewer_name = []
     comments = []
-    lines = []
-    read_csv(name, id1, host_id, host_name, neighbourhood, room_type, price, minimum_nights, availability_365, listing_id, id2, date, reviewer_id, reviewer_name, comments, lines)
-    create_listing(name, id1, host_id, host_name, neighbourhood, room_type, price, minimum_nights, availability_365,lines)
-    create_reviews(listing_id, id2, date, reviewer_id, reviewer_name, comments, lines)
+    read_csv(name, id1, host_id, host_name, neighbourhood, room_type, price, minimum_nights, availability_365, listing_id, id2, date, reviewer_id, reviewer_name, comments)
+    create_listing(name, id1, host_id, host_name, neighbourhood, room_type, price, minimum_nights, availability_365)
+    create_reviews(listing_id, id2, date, reviewer_id, reviewer_name, comments)
 
 if __name__ == "__main__":
     main()
