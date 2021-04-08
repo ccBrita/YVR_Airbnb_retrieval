985d0e50ab529c95eff4f78b21a6ef79d904633c
ccBrita <xuanhao@ualberta.ca>
2021-04-08 06:59:43 -0600

    try fix A5T2

--- A5T2.py
+++ A5T2.py
@@ -5,24 +5,25 @@ client = MongoClient("mongodb://localhost:27017")
 #connect to mongo serve
 mydb = client["A5db"]
 
-def create_listing(name, id1, host_id, host_name, neighbourhood, room_type, price, minimum_nights, availability_365):
+def create_listing(name, id1, host_id, host_name, neighbourhood, room_type, price, minimum_nights, availability_365,reviews):
     global client
     global mydb
-    col = mydb["listing"]
+    col = mydb["listings"]
     i = 0
     while (i < len(id1)):
-        listing = {"id":id1[i], "name":name[i], "host_id":host_id[i], "host_name":host_name[i], "neighbourhood":neighbourhood[i], "room_type":room_type[i], "price":price[i], "minmum_nights":minimum_nights[i],"availability_365": availability_365[i]}
-        col.insert_one(listing)
+        listing = {"id":id1[i], "name":name[i], "host_id":host_id[i], "host_name":host_name[i], "neighbourhood":neighbourhood[i], "room_type":room_type[i], "price":price[i], "minmum_nights":minimum_nights[i],"availability_365": availability_365[i],"reviews":reviews[i]}
+        col.insert_many(listing)
         i += 1
     
-def create_reviews(listing_id, id2, date, reviewer_id, reviewer_name, comments):
-    global client
-    global mydb
-    col = mydb["reviews"]
-    i = 0
-    while (i < len(id2)):
-        review = {"listing_id":listing_id[0], "id":id2[i], "date":date[i], "reviewer_id":reviewer_id[i], "reviewer_name":reviewer_name[i], "comments":comments[i]}
-        col.insert_one(review)
+def create_reviews(id1, listing_id, id2, date, reviewer_id, reviewer_name, comments, reviews):
+    while (i < len(id1)):
+        a = []
+        j = 0
+        while (j < len(id2)):
+            if (id1[i] == listing_id[j])：
+                a.append({"id":id2[i], "date":date[i], "reviewer_id":reviewer_id[i], "reviewer_name":reviewer_name[i], "comments":comments[i]})
+            j += 1
+        reviews.append(a)
         i += 1
 
 def read_csv(name, id1, host_id, host_name, neighbourhood, room_type, price, minimum_nights, availability_365, listing_id, id2, date, reviewer_id, reviewer_name, comments):
@@ -72,9 +73,10 @@ def main():
     reviewer_id = []
     reviewer_name = []
     comments = []
+    reviews = []
     read_csv(name, id1, host_id, host_name, neighbourhood, room_type, price, minimum_nights, availability_365, listing_id, id2, date, reviewer_id, reviewer_name, comments)
-    create_listing(name, id1, host_id, host_name, neighbourhood, room_type, price, minimum_nights, availability_365)
     create_reviews(listing_id, id2, date, reviewer_id, reviewer_name, comments)
+    create_listing(name, id1, host_id, host_name, neighbourhood, room_type, price, minimum_nights, availability_365)
 
 if __name__ == "__main__":
     main()
