bd54a3d88539e8275b00eddc07a69a86b7eb66a9
ccBrita <xuanhao@ualberta.ca>
2021-04-09 03:42:06 -0600

    fix

--- A5T4MongoDB.py
+++ A5T4MongoDB.py
@@ -4,9 +4,9 @@ def main():
     myclient = pymongo.MongoClient("mongodb://localhost:27017")
     mydb = myclient["A5db"]
     mycol = mydb["listings"]
-    hosts = mycol.find({"reviews":[]}).sort("listing_id").limit(10)
+    hosts = mycol.find({"reviews":[]}).sort("id").limit(10)
     for host in hosts:
-        print(host["listing_id"])
+        print(host["id"])
 
 main()
 
