16307be36baacc369129775b97331e4a1df36883
ccBrita <xuanhao@ualberta.ca>
2021-04-09 02:43:44 -0600

    try

--- A5T3MongoDB.py
+++ A5T3MongoDB.py
@@ -4,7 +4,7 @@ def main():
     myclient = pymongo.MongoClient("mongodb://localhost:27017")
     mydb = myclient["A5db"]
     mycol = mydb["listings"]
-    hosts = mycol.distinct("host_id").sort("host_id").limit(10)
+    hosts = mycol.sort("host_id").distinct("host_id").limit(10)
     
     for host in hosts:
         number = mycol.find({"host_id":host["host_id"]}).count()
