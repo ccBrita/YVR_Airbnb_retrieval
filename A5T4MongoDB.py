75bd9d0b0e6f5d019c7b235a2812e543fc6c32b6
ccBrita <xuanhao@ualberta.ca>
2021-04-09 03:38:20 -0600

    Add T4 mdb

---
+++ A5T4MongoDB.py
@@ -0,0 +1,12 @@
+import pymongo
+
+def main():
+    myclient = pymongo.MongoClient("mongodb://localhost:27017")
+    mydb = myclient["A5db"]
+    mycol = mydb["listings"]
+    hosts = mycol.find({"reviews":[]}).sort("listing_id").limit(10)
+    for host in hosts:
+        print(host)
+
+main()
+
